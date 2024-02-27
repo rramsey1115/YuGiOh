6bc10d8249b603c04c804e652d6170ef4884aa85
Ryan Ramsey <rramsey1115@gmail.com>
2024-02-27 16:37:47 -0600

    added basic site navigation

---
+++ client/src/components/myDecks/MyDecksList.js
@@ -0,0 +1,12 @@
+export const MyDecksList = ({ loggedInUser }) => {
+    return (
+        <section className="container">
+            <header className="header">
+                <h2>My Decks List</h2>
+            </header>
+            <section className="main">
+
+            </section>
+        </section>
+    )
+}
