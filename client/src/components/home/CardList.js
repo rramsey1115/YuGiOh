6e23f96780d3755a985348029e471a0fc6e0b763
Ryan Ramsey <rramsey1115@gmail.com>
2024-03-04 19:48:37 -0600

    added table to cardsList

---
+++ client/src/components/home/CardList.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { getAllCards } from '../../managers/cardManager';
+import "./CardList.css";
+import monster from "../../images/monster-card-template.png";
+import extra from "../../images/extra-deck-template.png";
+import spell from "../../images/spell-card-template.png";
+import trap from "../../images/trap-card-template.png";
+import back from "../../images/back-card-template.jpg";
+
+export const CardGrid = () => {
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => { getAndSetAllCards() }, [])
+
+    const getAndSetAllCards = () => { getAllCards().then(setCards) };
+
+    
+    return (
+        <table id="card-list-table" className='table-dark'>
+            <thead>
+                <tr>
+                    <td></td>
+                    <td><h4>Name</h4></td>
+                    <td><h4>Type</h4></td>
+                    <td><h4>Race</h4></td>
+                </tr>
+            </thead>
+            <tbody>  
+                {cards.map(card => {
+                    return (
+                        <tr key={card.id}>
+                            {card.type == "Normal Monster" || card.type == "Flip Effect Monster" || card.type == "Effect Monster"  ? <td><img className="card-icon" alt="card icon" src={monster}/></td> 
+                            : card.type == "Spell Card" ? <td><img className="card-icon" alt="card icon" src={spell}/></td>
+                            : card.type == "Fusion Monster" ? <td><img className="card-icon" alt="card icon" src={extra}/></td>
+                            : card.type == "Trap Card" ? <td><img className="card-icon" alt="card icon" src={extra}/></td>
+                            : <td><img className="card-icon" alt="card icon" src={back}/></td> }
+                            <td>{card.name}</td>
+                            <td>{card.type}</td>
+                            <td>{card.race}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    );
+}
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,4 +1,4 @@
-import { CardGrid } from "./CardGrid"
+import { CardGrid } from "./CardList"
 
 export const Home = ({ nloggedInUser }) => {
     return (
